851471d101d93a1462eba9147676df42858938bc
Sally Khalil <sek22@njit.edu>
2021-05-08 23:11:51 -0400

    Feature: created login page and updated app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,58 @@ import simplejson as json
 from flask import Flask, Response
 from flask import render_template
 
+from flask import Flask, url_for, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+
+def __init__(self, username, password):
+    self.username = username
+    self.password = password
+
+
+@app.route('/', methods=['GET'])
+def index():
+    if session.get('logged_in'):
+        return render_template('home.html')
+    else:
+        return render_template('index.html', message="Hello!")
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        try:
+            db.session.add(User(username=request.form['username'], password=request.form['password']))
+            db.session.commit()
+            return redirect(url_for('login'))
+        except:
+            return render_template('index.html', message="User Already Exists")
+    else:
+        return render_template('register.html')
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        u = request.form['username']
+        p = request.form['password']
+        data = User.query.filter_by(username=u, password=p).first()
+        if data is not None:
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+        return render_template('index.html', message="Incorrect Details")
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('index'))
 
 
 def calc_import() -> List[Dict]:
